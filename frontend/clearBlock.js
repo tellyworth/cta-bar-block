51e658f6e98a8a1b1877049c1cbd99c2d6826475
William Patton <will@pattonwebz.com>
2020-07-25 18:18:11 +0100

    Move the clearing JS to a /frontend/ directory and tweak clear timings

---
+++ frontend/clearBlock.js
@@ -0,0 +1,11 @@
+async function clearBlock(el) {
+  console.log('clearBlock:' + el );
+    var wrapperEl = el.closest('.pwwp--cta-bar');
+    if( wrapperEl !== undefined ) {
+      wrapperEl.classList.add('pwwp--cta-bar--fading');
+      wrapperEl.classList.add('pwwp--cta-bar--hidden');
+      await new Promise(p => setTimeout(p, 1200));
+      wrapperEl.parentNode.removeChild(wrapperEl);
+    }
+
+}
